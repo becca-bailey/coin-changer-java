09f001d269efffd21975afacd9ba52413967956a
Becca Nelson <beccanelson88@gmail.com>
2016-05-12 12:45:57 -0500

    Pass sixth test

--- src/CoinChanger.java
+++ src/CoinChanger.java
@@ -7,6 +7,11 @@ public class CoinChanger {
     public static ArrayList<Integer> makeChange(int cents) {
         ArrayList<Integer> change = new ArrayList<Integer>();
 
+        while (cents >= 10) {
+            change.add(10);
+            cents -= 10;
+        }
+
         while (cents >= 5) {
             change.add(5);
             cents -= 5;
